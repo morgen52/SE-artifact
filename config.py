9667dada776e25618b36c1c8cac6b45821bd0137
1271989088@qq.com <1271989008@qq.com>
2023-12-11 20:57:13 +0800

    fix config

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-PMD_PATH = "/home/data1/.pmd/bin/pmd"
-GITHUB_TOKEN = "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"
+PMD_PATH = "/the/path/to/your/bin/pmd"
+GITHUB_TOKEN = "ghp_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
