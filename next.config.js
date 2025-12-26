fb0cce9ec35eacf7eb7a13aedda50acac83102ea
Morgen Liu <1271989008@qq.com>
2025-12-26 23:11:37 +0800

    remove basedir

--- next.config.js
+++ next.config.js
@@ -3,12 +3,7 @@
 
 // module.exports = nextConfig
 
-
-const repoName = 'SE-artifact'
-const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   output: 'export',
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
   images: { unoptimized: true },
 }
