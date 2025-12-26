948ed942f8e85a7eae4511a21741e40c377d921a
Morgen Liu <1271989008@qq.com>
2025-12-26 23:19:15 +0800

    should add basePath because in github pages /SE-artifact

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@
 
 // module.exports = nextConfig
 
+const isProd = process.env.NODE_ENV === 'production'
+const repoName = 'SE-artifact'
+
 module.exports = {
   output: 'export',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
   images: { unoptimized: true },
 }
